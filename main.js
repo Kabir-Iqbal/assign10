a3997033c55ee1e469f8eb6b35b65728af236807
Kabir-iqbal <161919126+Kabir-Iqbal@users.noreply.github.com>
2024-03-25 19:52:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// 10. Adding Comments: 
+// Choose two of the programs you’ve written, and add at least one comment to each. If you don’t 
+// have anything specific to write because your programs are too simple at this point, just add your name 
+// and the current date at the top of each program file. Then write one sentence describing what the 
+// program does.
+var program1 = 5;
+var program2 = 10;
+var addition = program1 + program2;
+console.log(addition);
---
+++ main.ts
@@ -0,0 +1,13 @@
+// 10. Adding Comments: 
+// Choose two of the programs you’ve written, and add at least one comment to each. If you don’t 
+// have anything specific to write because your programs are too simple at this point, just add your name 
+// and the current date at the top of each program file. Then write one sentence describing what the 
+// program does.
+
+// Defined programes
+let program1 : number = 5
+let program2 : number =  10
+// Addition program 
+let addition : number = program1+program2
+// print result 
+console.log(addition);
